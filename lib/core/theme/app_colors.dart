9b466182ec363b2fef710df3f5f472d9156eb672
Rohan-Varghese02 <rohanvarghese02@gmail.com>
2025-09-24 13:20:25 +0530

    VS code testing

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const backgroundColor700 = Color(0xff0C3645);
+  static const backgroundColor500 = Color(0xff104B5F);
+  static const teritiaryColor500 = Color(0xffC6DABF);
+  static const accentColor300 = Color(0xffFFF5DC);
+  static const primaryColor700 = Color(0xff147A5C);
+  static const primaryColor500 = Color(0xff18936F);
+}
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:habitoo/core/theme/app_colors.dart';
+
+class AppTheme {
+  static final appTheme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppColors.backgroundColor700,
+  );
+}
